f54f5b8c4962f8fd67f6ce62cbf18bf0140a8781
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-03-09 00:36:18 +0200

    feat: init

---
+++ server/src/main/java/com/zbib/hiresync/HireSyncApplication.java
@@ -0,0 +1,14 @@
+package com.zbib.hiresync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireSyncApplication.class,
+                args);
+    }
+
+}
