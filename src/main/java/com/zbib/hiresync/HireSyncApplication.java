a8f33ee67b9341e9c9c1f8a6e8b808e147c52837
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-04-10 00:10:18 +0300

    fix(dep): update dependency for OWASP

--- src/main/java/com/zbib/hiresync/HireSyncApplication.java
+++ src/main/java/com/zbib/hiresync/HireSyncApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @SuppressWarnings("PMD")
 public class HireSyncApplication {
-//
+
   public static void main(String[] args) {
     SpringApplication.run(HireSyncApplication.class, args);
   }
