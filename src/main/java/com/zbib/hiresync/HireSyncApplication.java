f5e31f5b41fc948b140292999e3887cffbd41189
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-04-09 23:05:25 +0300

    test

--- src/main/java/com/zbib/hiresync/HireSyncApplication.java
+++ src/main/java/com/zbib/hiresync/HireSyncApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HireSyncApplication {
 
+  /**
+   * Private constructor to prevent instantiation of this class.
+   * This addresses the PMD "UseUtilityClass" warning.
+   */
+  private HireSyncApplication() {
+    // This class should not be instantiated
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(HireSyncApplication.class, args);
   }
