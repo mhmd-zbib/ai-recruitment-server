ee1c8899387940a887e63f4cb83422c2f5efd125
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-04-11 23:10:35 +0300

    fix(auth): simplify builder annotations to fix linting issues

--- src/main/java/com/zbib/hiresync/entity/User.java
+++ src/main/java/com/zbib/hiresync/entity/User.java
@@ -2,7 +2,6 @@ package com.zbib.hiresync.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
